0128eef3ebf876a05b2f18e4e50ebab658ab29b8
jacmorel <>
2024-03-05 19:03:32 -0600

    Fix onepassword utility test not passing the right server response

--- src/onepassword/client.py
+++ src/onepassword/client.py
@@ -6,6 +6,16 @@ log.basicConfig(level=log.DEBUG)
 
 
 def get_item_fields(vault, item_name, fields):
+    """
+    Retrieve the fields of an item from a vault
+    ASSUMPTION: 1password returns the info in the order it was requested.
+    If this is ever broken, the value will be in different order
+
+    :param vault:
+    :param item_name:
+    :param fields:
+    :return:
+    """
     formatted_fields = ",".join(["label=" + s for s in fields])
     secrets_json = subprocess.check_output(
         ["op", "item", "get", item_name,
@@ -13,4 +23,4 @@ def get_item_fields(vault, item_name, fields):
          "--vault", vault,
          "--format", "json"])
     secrets = json.loads(secrets_json)
-    return tuple(item['value'] for item in secrets)
+    return tuple(item["value"] for item in secrets)
--- src/onepassword/test_client.py
+++ src/onepassword/test_client.py
@@ -13,7 +13,30 @@ class TestGetItem(unittest.TestCase):
 
     @patch('subprocess.check_output')
     def test_get_item(self, mock_check_output):
-        mock_check_output.return_value = json.dumps({'username': USER, 'password': PWD}).encode()
+        onepassword_response = f"""[
+  {{
+    "id": "username",
+    "type": "STRING",
+    "purpose": "USERNAME",
+    "label": "username",
+    "value": "{USER}",
+    "reference": "op://ServiceAccount/Amazon/username"
+  }},
+  {{
+    "id": "password",
+    "type": "CONCEALED",
+    "purpose": "PASSWORD",
+    "label": "password",
+    "value": "{PWD}",
+    "reference": "op://ServiceAccount/Amazon/password",
+    "password_details": {{
+      "strength": "WEAK",
+      "history": ["1", "2", "3"]
+    }}
+  }}
+]"""
+
+        mock_check_output.return_value = onepassword_response.encode()
 
         actual_result = get_item_fields(VAULT, ITEM, ["username", "password"])
 
