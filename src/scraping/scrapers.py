e4cbc5f38895b2d6e5f8e220b48356085ee6b8ef
jacmorel <>
2024-03-08 00:27:44 -0600

    Improve test organization and add scraping of the elements of the top of the order detail page

--- src/scraping/scrapers.py
+++ src/scraping/scrapers.py
@@ -1,46 +1,40 @@
+import logging as log
+import re
+from datetime import datetime
+
 from selenium.webdriver.chromium.webdriver import ChromiumDriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support.select import Select
-import logging as log
+
+ITEMS_SHIPPED = "Items shipped:"
+ITEMS_SHIPPED_LEN = len(ITEMS_SHIPPED)
 
 
 class Scraper:
     def __init__(self, driver: ChromiumDriver):
         self.driver = driver
 
-    def find_element_by_id(self, value: str) -> WebElement:
-        return self.driver.find_element(by=By.ID, value=value)
+    def find_element_by_id(self, value: str, parent: WebElement = None) -> WebElement:
+        return self.driver_if_null(parent).find_element(by=By.ID, value=value)
 
-    def find_element_by_id(self, parent: WebElement, value: str) -> WebElement:
-        return parent.find_element(by=By.ID, value=value)
+    def find_element_by_class(self, value: str, parent: WebElement = None) -> WebElement:
+        return self.driver_if_null(parent).find_element(by=By.CLASS_NAME, value=value)
 
-    def find_element_by_class(self, value: str) -> WebElement:
-        return self.driver.find_element(by=By.CLASS_NAME, value=value)
+    def find_element_by_css(self, value: str, parent: WebElement = None) -> WebElement:
+        return self.driver_if_null(parent).find_element(by=By.CSS_SELECTOR, value=value)
 
-    def find_element_by_class(self, parent: WebElement, value: str) -> WebElement:
-        return parent.find_element(by=By.CLASS_NAME, value=value)
-
-    def find_element_by_css(self, value: str) -> WebElement:
-        return self.driver.find_element(by=By.CSS_SELECTOR, value=value)
-
-    def find_element_by_css(self, parent: WebElement, value: str) -> WebElement:
-        return parent.find_element(by=By.CSS_SELECTOR, value=value)
-
-    def find_element_by_xpath(self, value: str) -> WebElement:
-        return self.driver.find_element(by=By.XPATH, value=value)
-
-    def find_element_by_xpath(self, parent: WebElement, value: str) -> WebElement:
-        return parent.find_element(by=By.XPATH, value=value)
+    def find_element_by_xpath(self, value: str, parent: WebElement = None) -> WebElement:
+        return self.driver_if_null(parent).find_element(by=By.XPATH, value=value)
 
     def find_elements_by_class(self, value: str) -> [WebElement]:
         return self.driver.find_elements(by=By.CLASS_NAME, value=value)
 
-    def find_elements_by_xpath(self, value: str) -> [WebElement]:
-        return self.driver.find_elements(by=By.XPATH, value=value)
+    def find_elements_by_xpath(self, value: str, parent: WebElement = None) -> [WebElement]:
+        return self.driver_if_null(parent).find_elements(by=By.XPATH, value=value)
 
-    def find_elements_by_xpath(self, parent: WebElement, value: str) -> [WebElement]:
-        return parent.find_elements(by=By.XPATH, value=value)
+    def driver_if_null(self, parent):
+        return parent if parent else self.driver
 
     def wait_for_input(self):
         input("Press Enter to continue: ")
@@ -85,6 +79,17 @@ class Order:
     def __repr__(self):
         return f"Order(number={self.order_number!r}, details={self.details_link!r}, invoice={self.invoice_link!r})"
 
+
+class Transaction:
+    def __init__(self, date, amount, cc_last_4):
+        self.date = date
+        self.amount = amount
+        self.cc_last_4 = cc_last_4
+
+    def __repr__(self):
+        return f"Transaction(date={self.date!r}, amount={self.amount!r}, cc_last_4={self.cc_last_4!r})"
+
+
 class OrderHistory(Scraper):
     def get_orders(self):
         self.driver.get("https://www.amazon.com/gp/your-account/order-history")
@@ -110,15 +115,14 @@ class OrderHistory(Scraper):
         return order
 
     def get_order_number(self, order_card):
-        element = self.find_element_by_css(order_card,
-                                           ".yohtmlc-order-id .a-color-secondary.value bdi")
+        element = self.find_element_by_css(".yohtmlc-order-id .a-color-secondary.value bdi", order_card)
         # value="//span[contains(., 'Order #')]/following-sibling::span/bdi")
         # value='//span[@class="a-color-secondary" and preceding-sibling::span[.="Order #"]]')
         # value='//span[preceding-sibling::span[.="Order #"]]')
         return element.text
 
     def get_order_details_link(self, order_card):
-        element = self.find_element_by_class(order_card, "yohtmlc-order-details-link")
+        element = self.find_element_by_class("yohtmlc-order-details-link", order_card)
         return element.get_property("href")
 
     def get_order_invoice_link(self, order_card):
@@ -126,3 +130,99 @@ class OrderHistory(Scraper):
         return element.get_property("href")
 
 
+class OrderDetail(Scraper):
+    def __init__(self, driver: ChromiumDriver, order=None):
+        super().__init__(driver)
+        self.order = order
+
+    def populate(self, order):
+        self.order = order
+        self.driver.get(order.details_link)
+
+    def get_summary(self):
+        results = self.find_elements_by_xpath('//div[@id="od-subtotals"]//div[@class="a-row"]')
+        details = {}
+        for result in results:
+            try:
+                detail_name = self.find_element_by_xpath('.//div[@class="a-column a-span7 a-text-left"]',
+                                                         result).text.strip()
+                detail_value = self.find_element_by_xpath('.//div[@class="a-column a-span5 a-text-right a-span-last"]',
+                                                          result).text.strip()
+                details[detail_name] = detail_value
+            except:
+                log.error("Problem getting summary of order {} on element \n {}",
+                          self.order.order_number.result.text.strip())
+                continue
+        return details
+
+    def get_details(self):
+        shipping_address = self.driver.find_element(By.CSS_SELECTOR, ".displayAddressDiv").text
+        payment_method = self.driver.find_element(By.CSS_SELECTOR,
+                                                  ".pmts-payments-instrument-detail-box-paystationpaymentmethod").text
+        item_subtotal = self.driver.find_element(By.XPATH,
+                                                 "//span[contains(text(), 'Item(s) Subtotal:')]/../../div[@class='a-column a-span5 a-text-right a-span-last']/span").text
+        shipping_handling = self.driver.find_element(By.XPATH,
+                                                     "//span[contains(text(), 'Shipping & Handling:')]/../../div[@class='a-column a-span5 a-text-right a-span-last']/span").text
+        total_before_tax = self.driver.find_element(By.XPATH,
+                                                    "//span[contains(text(), 'Total before tax:')]/../../div[@class='a-column a-span5 a-text-right a-span-last']/span").text
+        estimated_tax = self.driver.find_element(By.XPATH,
+                                                 "//span[contains(text(), 'Estimated tax to be collected:')]/../../div[@class='a-column a-span5 a-text-right a-span-last']/span").text
+        grand_total = self.driver.find_element(By.XPATH,
+                                               "//span[contains(text(), 'Grand Total:')]/../../div[@class='a-column a-span5 a-text-right a-span-last']/span").text
+
+    def get_transactions(self):
+        ITEMS_SHIPPED_LEN
+        transactions_div = self.find_element_by_xpath("//span[contains(text(),'Transactions')]/../../div")
+        transactions_spans = self.find_elements_by_xpath("//div[contains(@class, 'a-expander-content')]//span",
+                                                         transactions_div)
+        lines = [e.get_attribute("outerText").strip().replace("\n", "") for e in transactions_spans]
+        transactions = []
+        i = 0
+        while i < len(lines):
+            line = lines[i]
+            if line.startswith("Refund:"):
+                tx = self.extract_refund(line)
+            elif line.startswith(ITEMS_SHIPPED):
+                if line == ITEMS_SHIPPED:
+                    i += 1
+                    line = lines[i]
+                else:
+                    line = line.replace(ITEMS_SHIPPED)
+                tx = self.extract_payment(line)
+            transactions.append(tx)
+            i += 1
+        return transactions
+
+    def extract_refund(self, line):
+        date_match = self.match_date(line)
+        amount_match = self.match_amount(line)
+        if not date_match or not amount_match:
+            log.error("Could not parse Refund for order %s: '%s'", self.order.order_number, line)
+            return Transaction(None, 0, "Unparseable refund: " + line)
+        date_obj = datetime.strptime(date_match.group(1), '%B %d, %Y')
+        amount_float = float(amount_match.group(1))
+        return Transaction(date_obj, amount_float, None)
+
+    def extract_payment(self, line):
+        date_match = self.match_date(line)
+        amount_match = self.match_amount(line)
+        cc_last_4_match = self.match_credit_card(line)
+        if not date_match or not amount_match or not cc_last_4_match:
+            log.error("Could not parse Payment for order %s: '%s'", self.order.order_number, line)
+            return Transaction(None, 0, "Unparseable payment: " + line)
+        date_obj = datetime.strptime(date_match.group(1), '%B %d, %Y')
+        amount_float = float(amount_match.group(1))
+        cc_last_4 = cc_last_4_match.group(1)
+        return Transaction(date_obj, -amount_float, cc_last_4)
+
+    @staticmethod
+    def match_date(s: str):
+        return re.search(r'(\w+ \d{1,2}, \d{4})', s)
+
+    @staticmethod
+    def match_amount(s: str):
+        return re.search(r'\$(\d+\.\d{2})', s)
+
+    @staticmethod
+    def match_credit_card(s: str):
+        return re.search(r'ending in (\d{4}):', s)
--- src/scraping/test_scrapers.py
+++ src/scraping/test_scrapers.py
@@ -1,10 +1,11 @@
 import logging as log
 import os
+from datetime import datetime
 
 import pytest
 from selenium import webdriver
 
-from scraping.scrapers import OrderHistory, Order
+from scraping.scrapers import OrderHistory, Order, OrderDetail, Transaction
 
 log.basicConfig(level=log.INFO)
 
@@ -20,8 +21,7 @@ def driver():
 
 
 def test_get_current_page_orders(driver):
-    test_file_path = os.path.join(BASE_DIR, 'test_pages', 'order_history', 'order_history_p1.html')
-    driver.get(f'file://{test_file_path}')
+    load_page(driver, ['order_history', 'order_history_p1.html'])
 
     orders = OrderHistory(driver).get_current_page_orders()
 
@@ -55,4 +55,68 @@ def test_get_current_page_orders(driver):
                        Order('112-9722039-7405835',
                              'https://www.amazon.com/gp/your-account/order-details/ref=ppx_yo_dt_b_order_details_o09?ie=UTF8&orderID=112-9722039-7405835',
                              'https://www.amazon.com/gp/css/summary/print.html/ref=ppx_yo_dt_b_invoice_o09?ie=UTF8&orderID=112-9722039-7405835')]
-    assert [vars(o) for o in expected_orders] == [vars(o) for o in orders]
+    assert_object_arrays_equal(expected_orders, orders)
+
+
+def test_get_order_details_summary(driver):
+    load_page(driver, ['order_details', 'order_summary.html'])
+
+    summary = OrderDetail(driver, Order("1", None, None)).get_summary()
+
+    assert {'Estimated tax to be collected:': '$5.71',
+            'Grand Total:': '$74.98',
+            'Item(s) Subtotal:': '$69.27',
+            'Shipping & Handling:': '$0.00',
+            'Total before tax:': '$69.27'} == summary
+
+
+def test_get_order_details_transactions(driver):
+    load_page(driver, ['order_details', 'transactions.html'])
+
+    transactions = OrderDetail(driver, Order("1", None, None)).get_transactions()
+
+    assert_object_arrays_equal([
+        Transaction(datetime(2024, 2, 25, 0, 0), 22.59, None),
+        Transaction(datetime(2024, 2, 14, 0, 0), -55.04, '0002'),
+        Transaction(datetime(2024, 2, 14, 0, 0), -10.81, '0002'),
+    ], transactions)
+
+
+@pytest.mark.parametrize("line, expected", [
+    ["Refund: Completed February 25, 2024 - $22.59",
+     "Transaction(date=datetime.datetime(2024, 2, 25, 0, 0), amount=22.59, cc_last_4=None)"],
+    ["Refund:  25, 2024 - $22.59",
+     "Transaction(date=None, amount=0, cc_last_4='Unparseable refund: Refund:  25, 2024 - $22.59')"]
+])
+def test_extract_refund(driver, line, expected):
+    order_detail = OrderDetail(driver, Order(None, None, None))
+    tx = order_detail.extract_refund(line)
+    assert expected == str(tx)
+
+
+@pytest.mark.parametrize("line, expected", [
+    ["Items shipped: February 14, 2024 - Visa ending in 0002: $55.04",
+     "Transaction(date=datetime.datetime(2024, 2, 14, 0, 0), amount=-55.04, cc_last_4='0002')"],
+    ["February 14, 2024 - Visa ending in 0001: $5.05",
+     "Transaction(date=datetime.datetime(2024, 2, 14, 0, 0), amount=-5.05, cc_last_4='0001')"],
+    ["February 14, 2024 - Visa ending in : $55.04",
+     "Transaction(date=None, amount=0, cc_last_4='Unparseable payment: February 14, 2024 - Visa ending in : $55.04')"]
+])
+def test_extract_payment(driver, line, expected):
+    order_detail = OrderDetail(driver, Order(None, None, None))
+    tx = order_detail.extract_payment(line)
+    assert expected == str(tx)
+
+
+def load_page(driver, relative_path_elements):
+    path_elements = ['test_pages'] + relative_path_elements
+    test_file_path = os.path.join(BASE_DIR, *path_elements)
+    driver.get(f'file://{test_file_path}')
+
+
+def order(number="", details_link="", invoice_link=""):
+    return Order(number, details_link, invoice_link)
+
+
+def assert_object_arrays_equal(expected, actual):
+    assert [vars(o) for o in expected] == [vars(o) for o in actual]
