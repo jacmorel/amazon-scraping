7a25a29568d3a78834cdb6b2e4cde5a38e239439
jacmorel <>
2024-03-24 14:52:08 -0500

    Extend scraping DSL framework to support positional and hierarchical scraping

--- src/scraping/framework/dsl.py
+++ src/scraping/framework/dsl.py
@@ -1,11 +1,39 @@
-from selenium.webdriver.common.by import By
+from enum import Enum
+
+from selenium.webdriver.common.by import By as SelBy
 
 from utils.loggers import get_logger
 
-log = get_logger(__name__)
+log = get_logger()
 
 
 # noinspection SpellCheckingInspection
+
+def log_return_decorator(func):
+    def wrapper(*args, **kwargs):
+        log.info(f'{func.__name__}({args[0]})')
+        result = func(*args, **kwargs)
+        log.info(f'{func.__name__}({args[0]}) returned: {result}')
+        return result
+
+    return wrapper
+
+
+class By(Enum):
+    def __init__(self, by: str = None, get_by_value_func=None):
+        self.by = by
+        self.get_by_value_func = get_by_value_func
+
+    ID = (SelBy.CSS_SELECTOR, lambda tag, value: f"{tag}#{value}")
+    CLASS = (SelBy.CSS_SELECTOR, lambda tag, value: f"{tag}.{value}")
+    CSS = (SelBy.CSS_SELECTOR, lambda tag, value: value)
+    XPATH = (SelBy.XPATH, lambda tag, value: value)
+    TAG = (SelBy.TAG_NAME, lambda tag, value: tag)
+    POSITION = (SelBy.CSS_SELECTOR, lambda tag, value: f"{tag}:nth-of-type({value})")
+
+    def get_by_value(self, tag, value):
+        return self.by, self.get_by_value_func(tag, value)
+
 
 class Node:
     pass
@@ -16,31 +44,46 @@ class Variable(Node):
         self.attr = attr
         self.name = name
 
+    @log_return_decorator
     def scrape(self, element):
         attribute = element.get_attribute(self.attr)
         if attribute is None:
             log.error(f"No attribute {self.attr} found for {self.name}")
         return {self.name: attribute.strip()}
 
+    def __str__(self):
+        return f"Var({self.name}, {self.attr!s})"
+
 
 class Tag(Node):
-    def __init__(self, name, by: By = None, value: str = None, contents=None):
-        self.value = value
-        self.name = name
-        self.by = by
+    def __init__(self, tag, by: By = By.TAG, value=None, contents=None):
+        self.tag = tag
+        (self.by, self.value) = by.get_by_value(tag, value)
         self.children = contents if isinstance(contents, list) else [contents]
 
+    @log_return_decorator
     def scrape(self, parent):
-        elements = parent.find_elements(self.by, self.value)
+        elements = self.find_elements(parent)
         if (elements is None) or (len(elements) == 0):
             log.error(f"No elements found for {self}")
             return None
-        return [n.scrape(elements[0]) for n in self.children][0]
+        return {k: v for e in elements for n in self.children for k, v in n.scrape(e).items()}
+
+    def find_elements(self, parent):
+        try:
+            return parent.find_elements(self.by, self.value)
+        except Exception as e:
+            log.error(f"Unexpected error trying to find elements by {self.by}, {self.value}: " + str(e))
 
     def __str__(self):
-        return f"<{self.name} {self.by!s}='{self.value}'>"
+        return f"<{self.tag} {self.by!s}='{self.value}'>"
 
 
 class Div(Tag):
-    def __init__(self, by: By = None, value: str = None, contents=None):
+    def __init__(self, by: By = By.TAG, value=None, contents=None):
         super().__init__('div', by, value, contents)
+
+
+class A(Tag):
+    def __init__(self, by: By = By.TAG, value=None, contents=None):
+        super().__init__('a', by, value, contents)
--- tests/scraping/framework/test_dsl.py
+++ tests/scraping/framework/test_dsl.py
@@ -1,18 +1,83 @@
-from selenium.webdriver.common.by import By
-
-from scraping.framework.dsl import Div, Variable
-from testing.files import write_temp_file, read_file
+from scraping.amazon.scrapers import log
+from scraping.framework.dsl import Div, Variable, A, By
+from testing.files import write_temp_file
 
 
 def test_div(headless_driver):
-    body = """<div class="container">
-                <div class="item">
-                    Item
+    assert_scraping_equal(
+        headless_driver,
+        """
+        <div class="container">
+            <div class="item">
+                Item
+            </div>
+        </div>""",
+        Div(By.CLASS, "container",
+            Div(By.CLASS, "item",
+                Variable(name="item", attr="outerText")
+                )),
+        {"item": "Item"}
+    )
+
+
+def test_div_multiple_variable_under_one_tag(headless_driver):
+    assert_scraping_equal(
+        headless_driver,
+        """
+        <div class="container">
+            <div class="item">
+                <a href="http://link.com">
+                    Product title
+            </div>
+        </div>""",
+        Div(By.CLASS, "container",
+            Div(By.CLASS, "item",
+                A(contents=[
+                    Variable(name="title", attr="outerText"),
+                    Variable(name="link", attr="href")]
+                ))), {
+            "title": "Product title",
+            "link": "http://link.com/"
+        }
+    )
+
+
+def test_div_multiple_variable_under_multiple_tag(headless_driver):
+    assert_scraping_equal(
+        headless_driver,
+        """
+        <div class="container">
+            <div class="item">
+                <div>
+                  Beautiful Title
                 </div>
-            </div>"""
-    scraping_dsl = Div(By.CLASS_NAME, "container", Div(By.CLASS_NAME, "item", Variable(name="item", attr="outerText")))
-    expected = {"item": "Item"}
-    assert_scraping_equal(headless_driver, body, scraping_dsl, expected)
+                <div>
+                     Reference
+                </div>
+                <div>
+                     details
+                </div>
+            </div>
+            <div>
+                other 1
+            </div>
+            <div>
+                other 2
+            </div>
+        </div>""",
+        Div(By.CLASS, "container",
+            Div(By.CLASS, "item", [
+                Div(By.POSITION, 1,
+                    Variable(name="title", attr="outerText")
+                    ),
+                Div(By.POSITION, 2,
+                    Variable(name="reference", attr="outerText")
+                    )
+            ])), {
+            "title": "Beautiful Title",
+            "reference": "Reference"
+        }
+    )
 
 
 def assert_scraping_equal(driver, body, scraping_dsl, expected):
